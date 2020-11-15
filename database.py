088eea19d05fc883bcf940effd032d2e03c0030d
JaeHyunL <swaa23@gmail.com>
2020-11-15 12:07:29 +0900

    데이터베이스 클래스 업데이트

--- database.py
+++ database.py
@@ -1,11 +1,12 @@
 import psycopg2
 
-
 class Databases():
     def __init__(self):
-        self.db = psycopg2.connect(host='192.168.1.152', dbname='openapi',user='postgres',password='mysecretpassword',port=5433)
-        self.cursor = self.db.cursor()
-
+        try: 
+            self.db = psycopg2.connect(host='localhost', dbname='testdb',user='postgres',password='password',port=5432)
+            self.cursor = self.db.cursor()
+        except Exception as e :
+            print('연결실패 왤까',e)
     def __del__(self):
         self.db.close()
         self.cursor.close()
